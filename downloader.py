91fc9f475413b533d2a92923959cc99544297976
Jason Jacobs <56858451+Jayj30pro@users.noreply.github.com>
2022-12-03 19:00:04 -0800

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -10,14 +10,20 @@ Label(root,text = "YouTube downloader", font = 'arial 20 bold').pack()
 link =  StringVar()
 
 Label(root, text= "Paste Link Here:", font = 'arial 15 bold').place(x= 160 , y=60)
-link_enter = Entry(root, width = 70,textvariable = link).place(x =32, y = 90)
+link_enter = Entry(root, width = 70)
+link_enter.place(x =32, y = 90)
+
+def clearRow():
+    link_enter.delete(0, END)
+
 
 def Downloader():
-    url = YouTube(str(link.get()))
+    url = YouTube(str(link_enter.get()))
     video = url.streams.get_highest_resolution()
     video.download()
     Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180, y= 210)
 
-Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' , bg = 'green', padx = 2, command = Downloader).place(x=180, y = 150)
-
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' , bg = '#0AC', padx = 2, command = Downloader).place(x=220, y = 150)
+Button(root,text = 'Clear', font = 'arial 15 bold' , bg = '#0AC', padx = 2, command = clearRow).place(x=80, y = 150)
+Button(root,text = 'Quit', font = 'arial 15 bold' , bg='#0AC', command = root.destroy).place(x = 250, y = 200) 
 root.mainloop()
