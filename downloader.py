8d9f871c49e5184bcfc77a4d110ab2506d390892
Jason Jacobs <56858451+Jayj30pro@users.noreply.github.com>
2022-12-05 22:58:31 -0800

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -21,9 +21,10 @@ def Downloader():
     url = YouTube(str(link_enter.get()))
     video = url.streams.get_highest_resolution()
     video.download()
-    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180, y= 210)
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 20, y= 210)
+    link_enter.delete(0, END)
 
 Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' , bg = '#0AC', padx = 2, command = Downloader).place(x=220, y = 150)
 Button(root,text = 'Clear', font = 'arial 15 bold' , bg = '#0AC', padx = 2, command = clearRow).place(x=80, y = 150)
-Button(root,text = 'Quit', font = 'arial 15 bold' , bg='#0AC', command = root.destroy).place(x = 250, y = 200) 
+Button(root,text = 'Quit', font = 'arial 15 bold' , bg='#0AC', command = root.destroy).place(x = 280, y = 200) 
 root.mainloop()
